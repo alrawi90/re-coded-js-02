d83339c0259e029a78b0ad1291c1bc7c8662c459
Ali Alrawi <ali.developer90@gmail.com>
2017-03-03 21:49:36 +0300

    Done

--- js/homework.js
+++ js/homework.js
@@ -1,1 +1,22 @@
-// Code your solution here.
+var controller={
+	read:()=>{return ('span').val().split(" ").remoteAccess()},
+	makeSence:()=>{return false},
+    write:()=>{return ('textarea').val().split(" ").fixPsycho()}
+}
+ controller.makeSence() ?  "kind of madness" : "you still have your frying brain"
+$(document).ready(function(){
+    $("#btn1").click(function(){
+        $("#box").animate({height: "300px"});
+    });
+    $("#btn2").click(function(){
+        $("#box").animate({height: "100px"});
+    });
+});
+    $("#btn1").click(function(){
+        $("p").append(" <b>Appended text</b>.");
+    });
+
+    $("#btn2").click(function(){
+        $("ol").append("<li>Appended item</li>");
+    });
+
