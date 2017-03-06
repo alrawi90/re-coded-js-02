233983fd2681f4edb76df83f318a255d16b4bcbc
Ali Alrawi <ali.developer90@gmail.com>
2017-03-06 12:08:50 +0300

    Done

--- js/homework.js
+++ js/homework.js
@@ -1,22 +1,29 @@
-var controller={
-	read:()=>{return ('span').val().split(" ").remoteAccess()},
-	makeSence:()=>{return false},
-    write:()=>{return ('textarea').val().split(" ").fixPsycho()}
-}
- controller.makeSence() ?  "kind of madness" : "you still have your frying brain"
-$(document).ready(function(){
-    $("#btn1").click(function(){
-        $("#box").animate({height: "300px"});
-    });
-    $("#btn2").click(function(){
-        $("#box").animate({height: "100px"});
-    });
-});
-    $("#btn1").click(function(){
-        $("p").append(" <b>Appended text</b>.");
-    });
+var obj={
+    decode:()=>{
 
-    $("#btn2").click(function(){
-        $("ol").append("<li>Appended item</li>");
-    });
+        var secret=$('body').html().match(/(<span)\s(hidden="">)[\w\W\D\d](<\/span>)/gi)
+        secret=secret.join("").replace(/hidden=""/g,'')
+        $('body').html(secret)
+    },
+    encode:(str)=>{
+     var salt="/.,mnbvcxzasdfghjkl;'][poiuytrewq1234567890-=+_)(*&^%$#@!~?&".split("")
+     str=str.split('');
+     var encoded =str.map(s=>{
+      var random=()=>{
+        var r="";
+        for(var i=0 ;i<100;i++){
+            r+=`<span>${salt[Math.floor(Math.random()*salt.length)]}</span>`
+            }
+            return r;
+         }
+      return `<span hidden="">${s}</span>${random()}`
+       });
+     // console.log(encoded.join(""))
+      $('body').html(encoded.join(""))
 
+      }
+     }
+$(function () {
+
+obj.decode()
+})
